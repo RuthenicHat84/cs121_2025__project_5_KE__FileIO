8d48d2c0ea702c33a002cd5e35520e58529c574f
Kayden Ewers <kewers05@gmail.com>
2025-02-14 22:56:17 +0000

    finished code

--- fileio.cpp
+++ fileio.cpp
@@ -4,46 +4,65 @@
 #include <iostream>
 #include <sstream>
 
-
-int Math(int a, int b);
-
 int main(){
+
+    // variables for reading files
+    std::ifstream inFile;
+    std::string currentLine;
+    std::stringstream ss;
+
+    // variables for data storage (strings)
+    std::string sNum1;
+    std::string sNum2;
+    std::string text;
+
+    // variables for data storage (ints)
+    int a;
+    int b;
+
+
+    // open the file
+    inFile.open("data.csv");
+    
+    // while loop start
+    while(getline(inFile, currentLine)){
+
+        // clear string stream
+        ss.clear();
+        
+	// pass the current line into the stringstream
+        ss.str(currentLine);
 	
-	std::string sNum1;
-	std::string sNum2;
+	// store values into sperate variables
+        getline(ss, sNum1, ',');
+        getline(ss, sNum2, ',');
+        getline(ss, text);
 
-	std::ifstream inFile;
-	std::stringstream ss; 
-	std::string currentLine;
-	std::string text;
+	// clear string stream
+        ss.clear();
+        ss.str("");
 
-	int a;
-	int b;
-	// open file
-	inFile.open("data.csv");
-	// start while
-	while(getline(inFile, currentLine, ',')){
-		std::cout << currentLine << std::endl;
-		
-		ss.clear();
-		ss.str(currentLine);
-		
-		getline(ss, sNum1, ',');
-		getline(ss, sNum2, ',');
-		getline(ss, text);
+	// pass values through converter
+        ss << sNum1 << " " << sNum2;
+        ss >> a >> b;
+	
+	// get sum of values
+        int sum = a + b;
 
+	// for the length of sum, print the value stored in text
+	// for loop start
+        for (int i = 0; i < sum; i++){
+            std::cout << text << " ";
+        } // end for
+	
 
-		ss.clear();
-		ss.str("");
+	// new line
+        std::cout << std::endl;
 
-		ss << a << " " << b;
-		ss >> a >> b;
+    } // end while
+    inFile.close();
 
-		int sum = a + b;
+    return(0);
 
-		std::cout << sum << std::endl;
-	} // end while
-
-	inFile.close();
-	return 0;
 } // end main
+
